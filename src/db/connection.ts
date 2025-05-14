0f393e4b64f13c3a85004f61abbbc3bf2b7be2ea
cadicv2 <ezio1622@gmail.com>
2025-05-13 21:47:46 -0500

    cambios polvito

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,60 +1,65 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import pg from 'pg';
 
 dotenv.config();
 
+// Variable para la instancia única
 let siscardRevolutionInstance: Sequelize | null = null;
 
+// Función que devuelve siempre la misma instancia
 export function getSiscardRevolution(): Sequelize {
-  if (!siscardRevolutionInstance) {
-    console.log("Creando nueva instancia de Sequelize");
-    siscardRevolutionInstance = new Sequelize(
-      process.env.NombreBD1 || "",
-      process.env.UsuarioBD1 || "",
-      process.env.ClaveBD1 || "",
-      {
+if (!siscardRevolutionInstance) {
+  console.log("Creando nueva instancia de Sequelize");
+  siscardRevolutionInstance = new Sequelize(
+    process.env.NombreBD1 || "",
+    process.env.UsuarioBD1 || "",
+    process.env.ClaveBD1 || "",
+    {
+        dialectModule: pg,
+        dialect: 'postgres',
         host: process.env.IpBD1 || "",
         port: 5432,
-        dialect: "postgres",
         dialectOptions: {
           ssl: {
             require: true,
-            rejectUnauthorized: false,
+            rejectUnauthorized: false
           },
-        },
-        pool: {
-          max: 10,
-          min: 0,
-          acquire: 30000,
-          idle: 10000,
-        },
-        logging: false,
+          pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+          }
+        }
       }
-    );
-  }
+  );
+}
 
   return siscardRevolutionInstance;
 }
 
+// Obtener la instancia única
+export const SiscardRevolution = getSiscardRevolution();
+
+// Función para conectar
 export const connect = async () => {
   try {
-    const sequelize = getSiscardRevolution();
-    await sequelize.authenticate();
+    await SiscardRevolution.authenticate();
     console.log("Base de datos CCD online");
   } catch (error) {
-    console.error("Error al conectar a la base de datos:", error);  // Aumenta los detalles del error para depuración
     console.log("Base de datos CCD offline");
     throw error;
   }
 };
 
+// Función para cerrar la conexión
 export const close = async () => {
   if (siscardRevolutionInstance) {
     await siscardRevolutionInstance.close();
-    siscardRevolutionInstance = null;
+    siscardRevolutionInstance = null; // Se establece en null para asegurar que la próxima conexión cree una nueva instancia
     console.log("Conexión a base de datos cerrada");
   }
 };
 
-// 👉 Esta línea permite seguir usando `import db from "../../db/connection"`
-export default getSiscardRevolution();
+export default SiscardRevolution;
